356cae238691a6561c76a0a2769ffcfe7ffe98b2
bbachdev <bbach7139@gmail.com>
2024-11-24 09:28:29 -0600

    Add aria-labels to social links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
         </ul>
         <ul className={'flex flex-row space-x-5 items-center mx-auto py-4'}>
           <li>
-            <a href={'https://bsky.app/profile/barrybach.com'} target={'_blank'} onClick={toggleMenu}>
+            <a href={'https://bsky.app/profile/barrybach.com'} target={'_blank'} onClick={toggleMenu} aria-label='Go to Bluesky profile'>
               <svg width="27" height="27" viewBox="0 0 360 320" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M254.896 184.158C252.81 183.926 250.733 183.645 248.671 183.315C250.773 183.574 252.849 183.855 254.896 184.158Z" fill="white" />
                 <path d="M180 141.964C163.699 110.262 119.308 51.1817 78.0347 22.044C38.4971 -5.86834 23.414 -1.03207 13.526 3.43594C2.08093 8.60755 0 26.1785 0 36.5164C0 46.8542 5.66748 121.272 9.36416 133.694C21.5786 174.738 65.0603 188.607 105.104 184.156C107.151 183.852 109.227 183.572 111.329 183.312C109.267 183.642 107.19 183.924 105.104 184.156C46.4204 192.847 -5.69621 214.233 62.6582 290.33C137.848 368.18 165.705 273.637 180 225.702C194.295 273.637 210.76 364.771 295.995 290.33C360 225.702 313.58 192.85 254.896 184.158C252.81 183.926 250.733 183.645 248.671 183.315C250.773 183.574 252.849 183.855 254.896 184.158C294.94 188.61 338.421 174.74 350.636 133.697C354.333 121.275 360 46.8568 360 36.519C360 26.1811 357.919 8.61012 346.474 3.43851C336.586 -1.02949 321.503 -5.86576 281.965 22.0466C240.692 51.1843 196.301 110.262 180 141.964Z" fill="#e5e7eb" />
@@ -54,13 +54,13 @@ export default function Header() {
             </a>
           </li>
           <li>
-            <a href={'https://www.instagram.com/barry__bach'} target={'_blank'} onClick={toggleMenu}><InstagramIcon fontSize={'large'} /></a>
+            <a href={'https://www.instagram.com/barry__bach'} target={'_blank'} onClick={toggleMenu} aria-label='Go to Instagram profile'><InstagramIcon fontSize={'large'} /></a>
           </li>
           <li>
-            <a href={'https://www.youtube.com/channel/UC2_ONiCusUKZ85yqGMNKgJg'} target={'_blank'} onClick={toggleMenu}><YouTubeIcon fontSize={'large'} /></a>
+            <a href={'https://www.youtube.com/channel/UC2_ONiCusUKZ85yqGMNKgJg'} target={'_blank'} onClick={toggleMenu} aria-label='Go to YouTube channel'><YouTubeIcon fontSize={'large'} /></a>
           </li>
           <li>
-            <a href={'mailto:contact@barrybach.com'} target={'_blank'} onClick={toggleMenu}><EmailIcon fontSize={'large'} /></a>
+            <a href={'mailto:contact@barrybach.com'} target={'_blank'} onClick={toggleMenu} aria-label='Contact via email'><EmailIcon fontSize={'large'} /></a>
           </li>
         </ul>
       </div>
@@ -85,7 +85,7 @@ export default function Header() {
       <div className={'ml-8 hidden md:flex flex-row items-center'}>
         <ul className={'flex flex-row space-x-5'}>
           <li className={`flex items-center`}>
-            <a href={'https://bsky.app/profile/barrybach.com'} target={'_blank'} onClick={toggleMenu}>
+            <a href={'https://bsky.app/profile/barrybach.com'} target={'_blank'} onClick={toggleMenu} aria-label='Go to Bluesky profile'>
               <svg width="27" height="27" viewBox="0 0 360 320" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M254.896 184.158C252.81 183.926 250.733 183.645 248.671 183.315C250.773 183.574 252.849 183.855 254.896 184.158Z" fill="white" />
                 <path d="M180 141.964C163.699 110.262 119.308 51.1817 78.0347 22.044C38.4971 -5.86834 23.414 -1.03207 13.526 3.43594C2.08093 8.60755 0 26.1785 0 36.5164C0 46.8542 5.66748 121.272 9.36416 133.694C21.5786 174.738 65.0603 188.607 105.104 184.156C107.151 183.852 109.227 183.572 111.329 183.312C109.267 183.642 107.19 183.924 105.104 184.156C46.4204 192.847 -5.69621 214.233 62.6582 290.33C137.848 368.18 165.705 273.637 180 225.702C194.295 273.637 210.76 364.771 295.995 290.33C360 225.702 313.58 192.85 254.896 184.158C252.81 183.926 250.733 183.645 248.671 183.315C250.773 183.574 252.849 183.855 254.896 184.158C294.94 188.61 338.421 174.74 350.636 133.697C354.333 121.275 360 46.8568 360 36.519C360 26.1811 357.919 8.61012 346.474 3.43851C336.586 -1.02949 321.503 -5.86576 281.965 22.0466C240.692 51.1843 196.301 110.262 180 141.964Z" fill="#e5e7eb" />
@@ -93,13 +93,13 @@ export default function Header() {
             </a>
           </li>
           <li>
-            <a href={'https://www.instagram.com/barry__bach'} target={'_blank'}><InstagramIcon fontSize={'large'} /></a>
+            <a href={'https://www.instagram.com/barry__bach'} target={'_blank'}><InstagramIcon fontSize={'large'} aria-label='Go to Instagram profile' /></a>
           </li>
           <li>
-            <a href={'https://www.youtube.com/channel/UC2_ONiCusUKZ85yqGMNKgJg'} target={'_blank'}><YouTubeIcon fontSize={'large'} /></a>
+            <a href={'https://www.youtube.com/channel/UC2_ONiCusUKZ85yqGMNKgJg'} target={'_blank'}><YouTubeIcon fontSize={'large'} aria-label='Go to YouTube channel' /></a>
           </li>
           <li>
-            <a href={'mailto:contact@barrybach.com'} target={'_blank'}><EmailIcon fontSize={'large'} /></a>
+            <a href={'mailto:contact@barrybach.com'} target={'_blank'}><EmailIcon fontSize={'large'} aria-label='Contact via email' /></a>
           </li>
         </ul>
       </div>
